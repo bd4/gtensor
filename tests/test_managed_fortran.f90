e97d65e895689af9967be896b460280d37fb13f6
Bryce Allen <bdallen@uchicago.edu>
2022-03-16 07:53:33 -0400

    managed: use big array to reproduce segfault

--- tests/test_managed_fortran.f90
+++ tests/test_managed_fortran.f90
@@ -5,34 +5,47 @@ program test_managed_fortran
 
   integer :: i
   type(C_PTR) :: d_rhs, d_p_g
-  complex, dimension(:), contiguous, pointer :: rhs, p_g
-  complex, dimension(:), allocatable :: cfgamma
+  complex, dimension(:), contiguous, pointer :: f_rhs, f_p_g
+  complex, dimension(:,:,:,:,:,:), contiguous, pointer :: rhs, p_g
+  complex, dimension(:,:,:,:,:,:), allocatable :: cfgamma
   complex :: a_complex
   integer(C_SIZE_T) :: bytes
-  integer(C_INT) :: N
+  integer(C_INT) :: N, N1, N2, N3, N4, N5, N6
+
+  N1 = 35
+  N2 = 16
+  N3 = 24
+  N4 = 24
+  N5 = 16
+  N6 = 2
+  N = N1*N2*N3*N4*N5*N6
 
   ! allocate inputs
-  N = 16
-
   bytes = N * C_SIZEOF(a_complex)
 
   call gpuAllocateManaged(d_rhs, bytes)
   call gpuAllocateManaged(d_p_g, bytes)
 
-  allocate(cfgamma(N))
+  allocate(cfgamma(N1,N2,N3,N4,N5,N6))
 
-  call C_F_POINTER(d_rhs, rhs, [N])
-  call C_F_POINTER(d_p_g, p_g, [N])
+  call C_F_POINTER(d_rhs, f_rhs, [N])
+  call C_F_POINTER(d_p_g, f_p_g, [N])
+
+  call C_F_POINTER(d_rhs, rhs, [N1,N2,N3,N4,N5,N6])
+  call C_F_POINTER(d_p_g, p_g, [N1,N2,N3,N4,N5,N6])
+
+  cfgamma = 2 * N
 
   ! init inputs
   do i=1,N
-    cfgamma(i) = i * 1.0 / N
-    p_g(i) = 2 * N
+    f_p_g(i) = i * 1.0 / N
   end do
 
   ! compute result
+  ! segfaults in ifort unless stack size is set to unlimited:
+  ! ulimit -s unlimited
   rhs = 0.5 * cfgamma * p_g
 
-  print *,rhs
+  print *,f_rhs(1:12)
 
 end program test_managed_fortran
